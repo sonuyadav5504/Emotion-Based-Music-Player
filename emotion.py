993a2cb4a516256d68ccbe7afc1c982d885da44f
Sonu Yadav <85031957+sonuyadav5504@users.noreply.github.com>
2023-05-12 12:23:12 +0530

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -1,3 +1,9 @@
+# import libraries
+import os
+from tkinter import *
+from tkinter import filedialog
+from pygame import mixer
+
 import numpy as np
 import argparse
 # import matplotlib.pyplot as plt
@@ -14,98 +20,21 @@ from keras.preprocessing.image import ImageDataGenerator
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# command line argument
-ap = argparse.ArgumentParser()
-ap.add_argument("--mode",help="train/display")
-mode = ap.parse_args().mode
+# Create a GUI window
+root = Tk()
+root.title("Music Player")
+root.geometry("920x600+290+85")
+root.configure(background='#0f1a2b')
+root.resizable(False, False)
 
-# # plots accuracy and loss curves
-# def plot_model_history(model_history):
-#     """
-#     Plot Accuracy and Loss curves given the model_history
-#     """
-#     fig, axs = plt.subplots(1,2,figsize=(15,5))
-#     # summarize history for accuracy
-#     axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
-#     axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
-#     axs[0].set_title('Model Accuracy')
-#     axs[0].set_ylabel('Accuracy')
-#     axs[0].set_xlabel('Epoch')
-#     axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
-#     axs[0].legend(['train', 'val'], loc='best')
-#     # summarize history for loss
-#     axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
-#     axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
-#     axs[1].set_title('Model Loss')
-#     axs[1].set_ylabel('Loss')
-#     axs[1].set_xlabel('Epoch')
-#     axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
-#     axs[1].legend(['train', 'val'], loc='best')
-#     fig.savefig('plot.png')
-#     plt.show()
+mixer.init()
 
-# Define data generators
-train_dir = 'train'
-val_dir = 'test'
+# Create a function to open a file
 
-num_train = 28709
-num_val = 7178
-batch_size = 64
-num_epoch = 20
 
-train_datagen = ImageDataGenerator(rescale=1./255)
-val_datagen = ImageDataGenerator(rescale=1./255)
+def AddMusic():
 
-train_generator = train_datagen.flow_from_directory(
-        train_dir,
-        target_size=(48,48),
-        batch_size=batch_size,
-        color_mode="grayscale",
-        class_mode='categorical')
-
-validation_generator = val_datagen.flow_from_directory(
-        val_dir,
-        target_size=(48,48),
-        batch_size=batch_size,
-        color_mode="grayscale",
-        class_mode='categorical')
-
-# Create the model
-model = Sequential()
-
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(7, activation='softmax'))
-
-# If you want to train the same model or try other models, go for this
-if mode == "train":
-    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
-    model_info = model.fit_generator(
-            train_generator,
-            steps_per_epoch=num_train // batch_size,
-            epochs=num_epoch,
-            validation_data=validation_generator,
-            validation_steps=num_val // batch_size)
-    # plot_model_history(model_info)
-    model.save_weights('model.h5')
-
-# emotions will be displayed on your face from the webcam feed
-elif mode == "display":
-    model.load_weights('model.h5')
-
-    # prevents openCL usage and unnecessary logging messages
+        # prevents openCL usage and unnecessary logging messages
     cv2.ocl.setUseOpenCL(False)
 
     # dictionary which assigns each label an emotion (alphabetical order)
@@ -146,9 +75,106 @@ elif mode == "display":
     cv2.destroyAllWindows()
     time.sleep(2)
 
-    #play music
-    music=playsound(p+".mp3")
+    # #play music
+    # music=playsound(p+".mp3")
+    Playlist.insert(0, 'Songs/'+p+'.mp3')
 
-           
+    # path = filedialog.askdirectory()
+    # if path:
+    #     os.chdir(path)
+    #     songs = os.listdir(path)
 
-    
+    #     for song in songs:
+    #         if song.endswith(".mp3"):
+    #             Playlist.insert(END, song)
+
+
+def PlayMusic():
+    Music_Name = Playlist.get(ACTIVE)
+    print(Music_Name[0:-4])
+    mixer.music.load(Playlist.get(ACTIVE))
+    mixer.music.play()
+
+
+# icon
+image_icon = PhotoImage(
+    file="./Images/logo.png")
+root.iconphoto(False, image_icon)
+
+Top = PhotoImage(
+    file="./Images/top.png")
+Label(root, image=Top, bg="#0f1a2b").pack()
+
+# logo
+logo = PhotoImage(
+    file="./Images/logo.png")
+Label(root, image=logo, bg="#0f1a2b", bd=0).place(x=70, y=115)
+
+# Button
+ButtonPlay = PhotoImage(
+    file="./Images/play.png")
+Button(root, image=ButtonPlay, bg="#0f1a2b", bd=0,
+       command=PlayMusic).place(x=100, y=400)
+
+ButtonStop = PhotoImage(
+    file="./Images/stop.png")
+Button(root, image=ButtonStop, bg="#0f1a2b", bd=0,
+       command=mixer.music.stop).place(x=30, y=500)
+
+ButtonResume = PhotoImage(
+    file="./Images/resume.png")
+Button(root, image=ButtonResume, bg="#0f1a2b", bd=0,
+       command=mixer.music.unpause).place(x=115, y=500)
+
+ButtonPause = PhotoImage(
+    file="./Images/pause.png")
+Button(root, image=ButtonPause, bg="#0f1a2b", bd=0,
+       command=mixer.music.pause).place(x=200, y=500)
+
+# Label
+Menu = PhotoImage(
+    file="./Images/menu.png")
+Label(root, image=Menu, bg="#0f1a2b").pack(padx=10, pady=50, side=RIGHT)
+
+Frame_Music = Frame(root, bd=2, relief=RIDGE)
+Frame_Music.place(x=330, y=350, width=560, height=200)
+
+Button(root, text="Emotion_Detection", width=15, height=2, font=("arial",
+       10, "bold"), fg="Black", bg="#21b3de", command=AddMusic).place(x=330, y=300)
+
+Scroll = Scrollbar(Frame_Music)
+Playlist = Listbox(Frame_Music, width=100, font=("Aloja", 10), bg="#000000",
+                   fg="white", selectbackground="lightblue", cursor="hand2", bd=0, yscrollcommand=Scroll.set)
+Scroll.config(command=Playlist.yview)
+Scroll.pack(side=RIGHT, fill=Y)
+Playlist.pack(side=LEFT, fill=BOTH)
+
+# Create the model
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+model.load_weights('model.h5')
+
+# Execute Tkinter
+root.mainloop()
+
+
+
+
+
+
